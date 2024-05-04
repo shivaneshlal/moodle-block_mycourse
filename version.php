da3f4c406a4c66d0e72fe5fcdae6c3dfc7a5442f
shivaneshlal <shivanesh.lal@outlook.com>
2024-05-04 05:52:59 +0000

    Updated the git version

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_mycourse';
 $plugin->release = '1.1';
-$plugin->version = 2024050106;
+$plugin->version = 2024050107;
 $plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_STABLE;
